ac991d9bcd43d3527458bf3b152d1bb87ca1939c
Suwatjanee Sae-aung <58070156@it.kmitl.ac.th>
2020-02-09 18:56:50 +0700

    test plot graph
    Test plot graph but it's wrong.

--- 2DPCA/face_recognition.m
+++ 2DPCA/face_recognition.m
@@ -7,11 +7,29 @@ path = ['angle']; %dataset
 TestImage = ['1'] 
 
 %%%%%%%%%%%%%  calling the functions  %%%%%%%%%%%%%%%%%%%%%%%%
-pc = 1; % number of eigenface
+
+escs = [];
+scs=[];
+for i = 1:10
+pc = i; % number of eigenface
 px = 0; %% 10k mod
 
 %%%%%%%%%%%% 2DPCA %%%%%%%%%%%%%%%%%%%
-[featureTr2 featureTs2 lblTr2 lblTs2 nameTr2 nameTs2] = eigenfaceExtractByUsing2DPCA(path,40,pc,px);
+[featureTr2 featureTs2 lblTr2 lblTs2 nameTr2 nameTs2, Eivecs, imIn] = eigenfaceExtractByUsing2DPCA(path,40,pc,px);
 lblPredict2 = knnclassify(featureTs2', featureTr2', lblTr2, 1);
 
 score2_2DPCA = sum(lblTs2 == lblPredict2)/size(lblTs2,1)*100
+
+escs = [i score2_2DPCA];
+scs = [scs; escs];
+end
+x=[];
+y=[];
+for u = 1: size(scs)
+    x = [x; scs(u, 1)];
+    y = [y; scs(u, 2)];
+end
+
+ylabel('scores');
+xlabel('amount of eigenface');
+plot(x, y);
