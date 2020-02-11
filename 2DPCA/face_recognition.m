7a5aecf15866d9cf92b0587fb9bf0f240b9ff697
SuwatjSA <60727281+SuwatjSA@users.noreply.github.com>
2020-02-12 05:29:52 +0700

    update plot
    - add multiple plot
    - add px plot

--- 2DPCA/face_recognition.m
+++ 2DPCA/face_recognition.m
@@ -4,32 +4,70 @@ close all
 clc
 
 path = ['angle']; %dataset
-TestImage = ['1'] 
+TestImage = [''] 
 
 %%%%%%%%%%%%%  calling the functions  %%%%%%%%%%%%%%%%%%%%%%%%
 
-escs = [];
-scs=[];
-for i = 1:10
-pc = i; % number of eigenface
-px = 0; %% 10k mod
+scorePerEi = [];
+for i = 0: 9
+    scs=[];
+    %%%%%% Size V %%%%%%
+    %%% angle = 92 %%%
+    %%% emoji = 195 %%%
+    %%% illumination = 168 %%%
+    
+    %%%%% Edit range with Size V (from above)%%%%%
+    for j = 1: 92 %1: 5
+        pc = j; % number of eigenface
+        px = i; %% 10k mod
 
-%%%%%%%%%%%% 2DPCA %%%%%%%%%%%%%%%%%%%
-[featureTr2 featureTs2 lblTr2 lblTs2 nameTr2 nameTs2, Eivecs, imIn] = eigenfaceExtractByUsing2DPCA(path,40,pc,px);
-lblPredict2 = knnclassify(featureTs2', featureTr2', lblTr2, 1);
+        %%%%%%%%%%%% 2DPCA %%%%%%%%%%%%%%%%%%%
+        [featureTr2 featureTs2 lblTr2 lblTs2 nameTr2 nameTs2, V] = eigenfaceExtractByUsing2DPCA(path,40,pc,px);
+        lblPredict2 = knnclassify(featureTs2', featureTr2', lblTr2, 1);
 
-score2_2DPCA = sum(lblTs2 == lblPredict2)/size(lblTs2,1)*100
+        score2_2DPCA = sum(lblTs2 == lblPredict2)/size(lblTs2,1)*100
 
-escs = [i score2_2DPCA];
-scs = [scs; escs];
+        %escs = [j score2_2DPCA];
+        scs = [scs; score2_2DPCA];
+    end
+    scorePerEi = cat(3, scorePerEi, scs);
 end
-x=[];
-y=[];
-for u = 1: size(scs)
-    x = [x; scs(u, 1)];
-    y = [y; scs(u, 2)];
+
+%%%%%%%%%% Plot graph %%%%%%%%%%
+% x=[];
+% y=[];
+% for u = 1: size(scs)
+%     x = [x; scs(u, 1)];
+%     y = [y; scs(u, 2)];
+% end
+% title('PX = 0');
+% ylabel('scores');
+% xlabel('amount of eigenvector');
+% plot(x, y);
+
+%%%%%%%%%% Multiple plot %%%%%%%%%%
+x = 1: size(V,1); %1:5;
+for k = 1: 10 %1: 3
+    y=[];
+    for m = 1: size(V,1) %1: 5
+        y = [y, scorePerEi(m,1,k)];
+    end
+    subplot(6,2,k)
+    plot(x, y)
+    title(['PX = ', num2str(k-1)])
 end
 
-ylabel('scores');
-xlabel('amount of eigenface');
-plot(x, y);
+meanScore = [];
+chkSum = [];
+for n = 1: size(V,1) %1: 5
+    sumScore = 0;
+    for o = 1: 10 %1: 3 
+        sumScore = sumScore + scorePerEi(n,1,o);
+    end
+    chkSum = [chkSum; sumScore];
+    meanScore = [meanScore; sumScore/o];
+end
+subplot(6,2,11)
+plot(x, meanScore)
+title('PX = 0-9')
+    
