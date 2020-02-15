5786c9059676fea08683825c99a6fe383df05081
SuwatjSA <60727281+SuwatjSA@users.noreply.github.com>
2020-02-15 16:19:44 +0700

    add each px for illu

--- 2DPCA/face_recognition.m
+++ 2DPCA/face_recognition.m
@@ -6,20 +6,11 @@ clc
 path = ['illumination']; %dataset
 TestImage = [''] 
 
-%%%%%%%%%%%%%  calling the functions  %%%%%%%%%%%%%%%%%%%%%%%%
-
-scorePerEi = [];
-for i = 0: 2 %%%% change px here %%%% %9
-    scs=[];
-    %%%%%% Size V %%%%%%
-    %%% angle = 92 %%%
-    %%% emoji = 195 %%%
-    %%% illumination = 168 %%%
-    
-    %%%%% Edit range with Size V (from above)%%%%%
-    for j = 1: 15 %168 %1: 5
+scs=[];
+%%%%%% For illumination%%%%%%
+for j = 1: 168 %168 %1: 5
         pc = j; % number of eigenface
-        px = i; %% 10k mod
+        px = 1; %% 10k mod
 
         %%%%%%%%%%%% 2DPCA %%%%%%%%%%%%%%%%%%%
         [featureTr2 featureTs2 lblTr2 lblTs2 nameTr2 nameTs2, V] = eigenfaceExtractByUsing2DPCA(path,38,pc,px);
@@ -30,8 +21,33 @@ for i = 0: 2 %%%% change px here %%%% %9
         %escs = [j score2_2DPCA];
         scs = [scs; score2_2DPCA];
     end
-    scorePerEi = cat(3, scorePerEi, scs);
-end
+
+%%%%%%%%%%%%%  calling the functions  %%%%%%%%%%%%%%%%%%%%%%%%
+
+% scorePerEi = [];
+% for i = 0: 2 %%%% change px here %%%% %9
+%     scs=[];
+%     %%%%%% Size V %%%%%%
+%     %%% angle = 92 %%%
+%     %%% emoji = 195 %%%
+%     %%% illumination = 168 %%%
+%     
+%     %%%%% Edit range with Size V (from above)%%%%%
+%     for j = 1: 15 %168 %1: 5
+%         pc = j; % number of eigenface
+%         px = i; %% 10k mod
+% 
+%         %%%%%%%%%%%% 2DPCA %%%%%%%%%%%%%%%%%%%
+%         [featureTr2 featureTs2 lblTr2 lblTs2 nameTr2 nameTs2, V] = eigenfaceExtractByUsing2DPCA(path,38,pc,px);
+%         lblPredict2 = knnclassify(featureTs2', featureTr2', lblTr2, 1);
+% 
+%         score2_2DPCA = sum(lblTs2 == lblPredict2)/size(lblTs2,1)*100
+% 
+%         %escs = [j score2_2DPCA];
+%         scs = [scs; score2_2DPCA];
+%     end
+%     scorePerEi = cat(3, scorePerEi, scs);
+% end
 
 %%%%%%%%%% Plot graph %%%%%%%%%%
 % x=[];
@@ -46,18 +62,18 @@ end
 % plot(x, y);
 
 %%%%%%%%%% Multiple plot %%%%%%%%%%
-x = 1: 15 %size(V,1); %1:5;
+%x = 1: 15 %size(V,1); %1:5;
 
 %%% Each PX %%%
-for k = 1: 3%10 %1: 3
-    y=[];
-    for m = 1: size(V,1) %1: 5
-        y = [y, scorePerEi(m,1,k)];
-    end
-    subplot(1,3,k)
-    plot(x, y)
-    title(['PX = ', num2str(k-1)])
-end
+% for k = 1: 3%10 %1: 3
+%     y=[];
+%     for m = 1: size(V,1) %1: 5
+%         y = [y, scorePerEi(m,1,k)];
+%     end
+%     subplot(1,3,k)
+%     plot(x, y)
+%     title(['PX = ', num2str(k-1)])
+% end
 
 %%% Mean PX %%%
 % meanScore = [];
