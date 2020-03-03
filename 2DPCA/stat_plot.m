5bd2b8ec79cd930275c4c74c165e3996d0835f90
SuwatjSA <60727281+SuwatjSA@users.noreply.github.com>
2020-03-04 02:17:01 +0700

    add test polyfit

--- 2DPCA/stat_plot.m
+++ 2DPCA/stat_plot.m
@@ -7,23 +7,29 @@ load('stat_vector_angle');
 %load('stat_vector_emoji');
 %load('stat_vector_illumination');
 
-%%%%% IMPORTANT PART %%%%%
-%%% Please note your old round to make sure you don't forget it
-%%% List old round = x,y,z
-
-% old_round = 0;
-% 
-% %%%%% start value = old round * 2 + 1
-% start = 1;
-% 
-% stop = 56;
-
 %%% All Render %%%
 % figure()
 % boxplot(all_stat0());
 % hold on
 % plot(vec_mean0(),'g');
 count = 0;
+
+%%%%% Polyfit %%%%%
+x = 1: size(vec_mean0, 2);
+%%% For angle %%%
+y = vec_mean0;
+%%% For emoji, illumination %%%
+%y = vec_min0;
+
+poly = polyfit(x, y, 1);
+poly_val = polyval(poly, y);
+
+%%%%% Test polyfit with boxplot %%%%%
+boxplot(all_stat0(:,501:550))
+hold on
+plot(vec_mean0(1,501:550),'g')
+hold on
+plot(poly_val(1,501:550), 'r')
 
 %%% Part Render %%%
 %%% Finish in 1 round %%%
@@ -109,19 +115,6 @@ count = count+1;
 % angle vector = 1 x 8646
 % emoji vector = 1 x 38025
 % illumination vector = 1 x 28224
-%%% figure size angle = 92/2 = [46, 46], round = 92*2 = 184
-%%% figure size emoji = 195/4 = [48, 49, 49, 49], round = 195*4 = 768
-%%% figure size illumination = 168/3 = [56, 56, 56], round = 168*3 = 504
-
-% ss = size(cov_mean0, 1);
-% list_start =[];
-% list_stop = [];
-% amount_vec = 46;
-% for i = 1: ss
-%     list_start = [list_start, amount_vec * (i-1) + 1];
-%     list_stop = [list_start, amount_vec * i];
-% end
-
 
 
 
